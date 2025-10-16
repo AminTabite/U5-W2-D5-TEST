9e5c2f4550401d3cb844bc66d733d6b833537f6d
AminTabite <Aminepicode13@gmail.com>
2025-10-16 23:14:29 +0200

    creazione base
    inizio

---
+++ src/main/java/amintabite/U5_W2_D5_TEST/U5W2D5TestApplication.java
@@ -0,0 +1,13 @@
+package amintabite.U5_W2_D5_TEST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W2D5TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W2D5TestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/amintabite/U5_W2_D5_TEST/U5W2D5TestApplicationTests.java
@@ -0,0 +1,13 @@
+package amintabite.U5_W2_D5_TEST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W2D5TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
